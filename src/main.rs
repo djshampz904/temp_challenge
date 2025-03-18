6905afe53aa7a7fbcf7277064581f15dff535314
djshmpz904 <martin.kimani@strathmore.edu>
2025-03-18 12:39:20 +0300

    completion

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,31 @@ use std::io;
 
 fn main() {
 
-    let mut user_input = String::new();
-    let mut user_choice = String::new();
     
-    println!("Enter the units you are converting from: example (F) to convert from Fahrenheit to celcius (F/C)");
-    io::stdin().read_line(&mut user_choice).expect("Failed to read line");
+    loop{
+        let mut user_choice = String::new();
+
+        println!("Enter the units you are converting from: example (F) to convert from Fahrenheit to celcius (F/C)");
+        io::stdin().read_line(&mut user_choice).expect("Failed to read line");
  
-    let choice_user = user_choice.trim().to_uppercase();
-    match choice_user.as_str() {
-        "F" => println!("User picked fahrenheit"),
-        "C" => println!("User picked celcius"),
-        _   => println!("Invalid choice: options F/C"),
-    };
+        let choice_user = user_choice.trim().to_uppercase();
+        match choice_user.as_str() {
+            "F" => println!("User picked fahrenheit"),
+            "C" => println!("User picked celcius"),
+            _   => println!("Invalid choice: options F/C"),
+        };
 
-    if choice_user == "F" {
-        println!("Enter number to convert from fahrenheit to celcius");
-        io::stdin().read_line(&mut user_input).expect("Failed to read line");
+        if choice_user == "F" {
+            
+            get_input_convert("fahrenheit", "celcius");
 
-        let mut num_units: i32 = user_input.trim().parse().expect("Failed to parse value");
-        let mut results = fahrenheit_celcius(num_units);
-        println!("{num_units}F is {results}C");
-    } else if choice_user == "C" {
-        println!("Enter number to convert from Celcius to fahrenheit");
-        io::stdin().read_line(&mut user_input).expect("Failed to parse value");
 
-        let mut num_units: i32 = user_input.trim().parse().expect("Failed to parse value");
-        let mut results = celcius_fahrenheit(num_units);
-        println!("{num_units}C is {results}F");
+        } else if choice_user == "C" {
+
+            get_input_convert("celcius", "fahrenheit");
+
+        }
     }
-
 
 }
 
@@ -45,3 +41,40 @@ fn celcius_fahrenheit(value: i32) -> i32 {
     let fahrenheit: i32 = (value * 9/5) + 32;
     return fahrenheit;
 }
+
+fn get_input_convert(from_unit: &str, to_unit: &str)  {
+    
+    let num_units: i32  = loop {
+        let mut user_input = String::new();
+
+        println!("Enter number to convert from {from_unit} to {to_unit}");
+        io::stdin().read_line(&mut user_input).expect("Failed to read line");
+
+        match user_input.trim().parse::<i32>() {
+            Ok(num) => break num,
+            Err(_e) => {
+                eprintln!("Invalid number");
+                
+            },
+        };
+    };
+
+
+    if from_unit == "fahrenheit" {
+
+        let results = fahrenheit_celcius(num_units);
+        println!("{num_units}F is {results}C");
+
+    } else if from_unit == "celcius" {
+
+        let results = celcius_fahrenheit(num_units);
+        println!("{num_units}C is {results}F");
+
+    } else {
+
+        println!("Invalid choice Try again");
+
+    }
+}
+
+
